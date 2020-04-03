cd92a391ecdd40ccb9a45df68500d25866a110d6
Max Isaev <isaevm2@yandex.ru>
2020-04-03 21:25:43 +0300

    test(src): Add unit tests and benchmarks for a collector

--- collector_test.go
+++ collector_test.go
@@ -1,9 +1,9 @@
 package sqlstat
 
 import (
+	"database/sql"
 	"testing"
-
-	"github.com/prometheus/client_golang/prometheus"
+	"time"
 )
 
 func TestNewCollector(t *testing.T) {
@@ -13,8 +13,108 @@ func TestNewCollector(t *testing.T) {
 	}
 }
 
+func TestNewCollector_withoutPanic(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			t.Error("unexpected panic", err)
+		}
+	}()
+
+	newCollector()
+}
+
+func BenchmarkNewCollector(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		newCollector()
+	}
+}
+
+func BenchmarkNewCollector_WithOptsAndDB(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		newCollector().withDB(&sql.DB{}).withOpts(Opts{
+			Namespace: "sql",
+			Subsystem: "stat",
+			Interval:  5 * time.Second,
+		})
+	}
+}
+
+func TestNewCollector_WithDB(t *testing.T) {
+	c := newCollector().withDB(&sql.DB{})
+	if c.DB == nil {
+		t.Error("must be initialized")
+	}
+}
+
+func TestNewCollector_WithDB_withoutPanic(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			t.Error("unexpected panic", err)
+		}
+	}()
+
+	newCollector().withDB(&sql.DB{})
+}
+
+func BenchmarkNewCollector_WithDB(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		newCollector().withDB(&sql.DB{})
+	}
+}
+
+func TestNewCollector_WithOpts(t *testing.T) {
+	c := newCollector().withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5,
+	})
+	if c == nil {
+		t.Error("must be initialized")
+	}
+
+	if c.Namespace != "sql" {
+		t.Errorf("expect: %s, get: %s", "sql", c.Namespace)
+	}
+
+	if c.Subsystem != "stat" {
+		t.Errorf("expect: %s, get: %s", "stat", c.Subsystem)
+	}
+
+	if c.Interval != 5 {
+		t.Errorf("expect: %d, get: %d", 5, c.Interval)
+	}
+}
+
+func TestNewCollector_WithOpts_withoutPanic(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			t.Error("unexpected panic")
+		}
+	}()
+
+	newCollector().withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
+}
+
+func BenchmarkNewCollector_WithOpts(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		newCollector().withOpts(Opts{
+			Namespace: "sql",
+			Subsystem: "stat",
+			Interval:  5 * time.Second,
+		})
+	}
+}
+
 func TestCollector_RegisterMetrics(t *testing.T) {
-	c := &collector{}
+	c := newCollector().withDB(&sql.DB{}).withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
 
 	if len(c.metrics) != 0 {
 		t.Errorf("expect: %d, get: %d", 0, len(c.metrics))
@@ -27,6 +127,119 @@ func TestCollector_RegisterMetrics(t *testing.T) {
 	}
 }
 
-func TestCollect(t *testing.T) {
-	metrics := make(chan<- map[string]prometheus.Metric)
+func TestCollector_RegisterOpenConnections(t *testing.T) {
+	c := newCollector().withDB(&sql.DB{}).withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
+
+	c.registerOpenConnections()
+	if len(c.metrics) != 1 {
+		t.Errorf("expect: %d, get: %d", 1, len(c.metrics))
+	}
+}
+
+func TestCollector_RegisterConnectionsInUse(t *testing.T) {
+	c := newCollector().withDB(&sql.DB{}).withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
+
+	c.registerConnectionsInUse()
+	if len(c.metrics) != 1 {
+		t.Errorf("expect: %d, get: %d", 1, len(c.metrics))
+	}
+}
+
+func TestCollector_RegisterConnectionsIdle(t *testing.T) {
+	c := newCollector().withDB(&sql.DB{}).withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
+
+	c.registerConnectionsIdle()
+	if len(c.metrics) != 1 {
+		t.Errorf("expect: %d, get: %d", 1, len(c.metrics))
+	}
+}
+
+func TestCollector_RegisterConnectionsWait(t *testing.T) {
+	c := newCollector().withDB(&sql.DB{}).withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
+
+	c.registerConnectionsWait()
+	if len(c.metrics) != 1 {
+		t.Errorf("expect: %d, get: %d", 1, len(c.metrics))
+	}
+}
+
+func TestCollector_RegisterConnectionsWaitDuration(t *testing.T) {
+	c := newCollector().withDB(&sql.DB{}).withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
+
+	c.registerConnectionsWaitDuration()
+	if len(c.metrics) != 1 {
+		t.Errorf("expect: %d, get: %d", 1, len(c.metrics))
+	}
+}
+
+func TestCollector_RegisterConnectionsMaxIdleClosed(t *testing.T) {
+	c := newCollector().withDB(&sql.DB{}).withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
+
+	c.registerConnectionsWaitDuration()
+	if len(c.metrics) != 1 {
+		t.Errorf("expect: %d, get: %d", 1, len(c.metrics))
+	}
+}
+
+func TestCollector_RegisterConnectionsMaxLifetimeClosed(t *testing.T) {
+	c := newCollector().withDB(&sql.DB{}).withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
+
+	c.registerConnectionsMaxLifetimeClosed()
+	if len(c.metrics) != 1 {
+		t.Errorf("expect: %d, get: %d", 1, len(c.metrics))
+	}
+}
+
+func TestCollector_RegisterMaxOpenConnections(t *testing.T) {
+	c := newCollector().withDB(&sql.DB{}).withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
+
+	c.registerMaxOpenConnections()
+	if len(c.metrics) != 1 {
+		t.Errorf("expect: %d, get: %d", 1, len(c.metrics))
+	}
+}
+
+func TestCollector_GetCollectors(t *testing.T) {
+	c := newCollector().withDB(&sql.DB{}).withOpts(Opts{
+		Namespace: "sql",
+		Subsystem: "stat",
+		Interval:  5 * time.Second,
+	})
+
+	collectors := c.getCollectors()
+	if len(collectors) != 8 {
+		t.Errorf("expect: %d, get: %d", 8, len(collectors))
+	}
 }
