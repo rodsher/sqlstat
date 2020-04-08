eed33aa63d26604d6e53bba6ce302341fb9cc2d3
Max Isaev <isaevm2@yandex.ru>
2020-04-08 21:04:04 +0300

    feat(build): Add coverage section

---
+++ scripts/coverage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+go get golang.org/x/tools/cmd/cover
+go get github.com/mattn/goveralls
+go test -v -covermode=count -coverprofile=coverage.out
+$HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
